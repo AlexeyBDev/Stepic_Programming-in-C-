0a07aefb2e2c81e1b7fe8b3d7bda87d6a4a49824
AlexeyBDev <jobabb82@yandex.ru>
2017-04-07 17:50:28 +0300

    Add project files.

---
+++ Week_2/2_2_6.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int foo()
+//{
+//	cout << "Hello" << endl;
+//	return 2;
+//}
+//
+//int bar()
+//{
+//	int *m[1];
+//	// подмена адреса возвращаемого значения вызовом новой функции
+//	m[3] = (int *)&foo; 
+//	return 1;
+//}
+//
+//int main()
+//{
+//	bar();
+//
+//	return 0;
+//}
